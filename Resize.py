32f52bf1521f1a16e1a1184e19c0b71da5bb0217
jeltoP193 <38156815+jeltoP193@users.noreply.github.com>
2020-02-15 20:34:31 +0100

    Hello world
    r

--- Resize.py
+++ Resize.py
@@ -1,1 +1,19 @@
-print("Hello World")
+from os import environ, path, chdir, listdir
+import sys
+from PIL import Image
+
+chdir(environ['USERPROFILE'] + "\Desktop\\Whale Challenge\\")
+
+
+path = "train/"
+dirs = listdir(path)
+
+def modify_image():
+    for folder in dirs:
+        pics = listdir(path + str(folder))
+        for pic in pics:
+            print(folder + "-" + pic)
+            #im = Image.open(path + folder + pic)
+
+
+modify_image()
