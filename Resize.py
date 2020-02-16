7e1e0208b42ca27301ce3c356b32cad36457b21d
jeltoP193 <38156815+jeltoP193@users.noreply.github.com>
2020-02-16 15:15:18 +0100

    Update Resize.py

--- Resize.py
+++ Resize.py
@@ -2,18 +2,21 @@ from os import environ, path, chdir, listdir
 import sys
 from PIL import Image
 
-chdir(environ['USERPROFILE'] + "\Desktop\\Whale Challenge\\")
+chdir(environ['USERPROFILE'] + "\\Desktop\\Whale Challenge\\")
 
 
-path = "train/"
-dirs = listdir(path)
+train_fldr = "train/"
+train_resized_fldr = "train_resized"
+raw_train_dirs = listdir(train_fldr)
+
+
 
 def modify_image():
-    for folder in dirs:
-        pics = listdir(path + str(folder))
+    for folder in raw_train_dirs:
+        pics = listdir(train_fldr + str(folder))
         for pic in pics:
-            print(folder + "-" + pic)
-            #im = Image.open(path + folder + pic)
-
+            im = Image.open(path + folder + "\\" + pic)
+            imResize = im.resize((224,224), Image.ANTIALIAS)
+            imResize.save(path.join(train_resized_fldr, folder, pic.replace(".jpg", "_resized.jpg"), 'JPEG', quality=90)
 
 modify_image()
