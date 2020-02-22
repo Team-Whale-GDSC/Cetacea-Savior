616048c3945d1bdef808e740ef137ba653440298
jeltoP193 <38156815+jeltoP193@users.noreply.github.com>
2020-02-22 19:18:04 +0100

    Update Resize.py

--- Resize.py
+++ Resize.py
@@ -6,7 +6,7 @@ from cv2 import imread, COLOR_RGB2GRAY, ADAPTIVE_THRESH_GAUSSIAN_C, MORPH_OPEN, 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-
+import tensorflow as tf
 
 chdir(environ['USERPROFILE'] + "\\Desktop\\Whale Challenge\\")
 
@@ -16,7 +16,6 @@ train_resized_fldr = "train_resized/"
 train_preprocessed_fldr = "train_preprocessed/"
 
 raw_dirs = listdir(raw_fldr)
-df = DataFrame(columns=["Picture", "Date"])
 
 def resize_images():
     for fldr in listdir(train_fldr):
@@ -28,7 +27,7 @@ def resize_images():
         for pic in pics:
             im = Image.open(path.join(train_fldr, fldr, pic))
             im = im.resize((224,224), Image.ANTIALIAS)
-            im.save(path.join(train_resized_fldr, fldr, pic.replace(".jpg", "_resized.jpg")), 'JPEG', quality=90)
+            im.save(path.join(train_resized_fldr, fldr, pic), 'JPEG', quality=90)
 
 
 def remove_sea():
@@ -44,34 +43,71 @@ def remove_sea():
             grey = cvtColor(img, COLOR_RGB2GRAY)
             ret, img = threshold(grey,50,255, ADAPTIVE_THRESH_GAUSSIAN_C)
             
-            imwrite(path.join(train_preprocessed_fldr, fldr, pic + "_regular.jpg"), img)
+            #imwrite(path.join(train_preprocessed_fldr, fldr, pic + "_regular.jpg"), img)
 
 
+            #acceptable kernels: (kernel1 = np.ones((1, 1), np.uint8))
 
+            kernel1 = np.empty([1, 1]) 
+            kernel1 = kernel1.fill(1)
+            img1 = morphologyEx(img, MORPH_OPEN, kernel1, iterations=1)
+            imwrite(path.join(train_preprocessed_fldr, fldr, pic), img1)
 
-            kernel1 = np.ones((1, 1), np.uint8)
-            img1 = morphologyEx(img, MORPH_OPEN, kernel1, iterations=2)
-            imwrite(path.join(train_preprocessed_fldr, fldr, pic + "_kernel1.jpg"), img1)
-
-            kernel2 = np.ones((2, 2), np.uint8)
-            img2 = morphologyEx(img, MORPH_OPEN, kernel2, iterations=2)
+            '''
+            kernel2 = np.empty([1, 1]) 
+            kernel2 = kernel2.fill(2)
+            img2 = morphologyEx(img, MORPH_OPEN, kernel2, iterations=1)
             imwrite(path.join(train_preprocessed_fldr, fldr, pic + "_kernel2.jpg"), img2)
+            
+            
+            kernel3 = np.empty([1, 1]) 
+            kernel3 = kernel3.fill(100)
 
-            kernel3 = np.ones((100, 100), np.uint8)
-            img3 = morphologyEx(img, MORPH_OPEN, kernel3, iterations=2)
+            img3 = morphologyEx(img, MORPH_OPEN, kernel3, iterations=1)
             imwrite(path.join(train_preprocessed_fldr, fldr, pic + "_kernel3.jpg"), img3)
+            '''
 
 
-
-def get_picture_dates():
+def get_picture_date_id_state():
+    train_df = DataFrame(columns=["picture", "id", "date", "d_or_a"])
     for fldr in listdir(raw_fldr):
         for pic in listdir(raw_fldr + fldr):
-            if pic.endswith("jpg"):
-                # Dates are always in this position but sometimes it's lacking a dash so we take first eight digits of the date as the date
+            if pic.endswith(".jpg"):
                 picdate = pic.split("-")[2][:8]
-                df.loc[len(df)] = [pic, picdate]
-    df['Date']= to_datetime(df['Date']) 
-    print(df)
+                train_df.loc[len(train_df)] = [pic, picdate, 1]
+    train_df['date'] = to_datetime(train_df['date'])
+    
+    
+    for fldr in listdir(train_preprocessed_fldr):
+        whale_id = str(fldr)
+        for pic in listdir(train_preprocessed_fldr + fldr):
+            train_df[train_df.picture == pic] = whale_id
+
+    print(train_df.head(2))
+
+
+
+
+'''
+def model(train_df):
+    train_datagen = tf.keras.preprocessing.image.ImageDataGenerator()
+    train_generator = train_datagen.flow_from_dataframe(train_df, 
+                    x_col='picture', 
+                    y_col='animal', 
+                    target_size=(100,100), #images resized
+                    batch_size=20,
+                    class_mode='binary')
+
+
+'''
+
+
+
+
+
+
+
+
 
 
 
@@ -81,7 +117,24 @@ def get_picture_dates():
 
 def main():
     #resize_images()
-    remove_sea()
+    #remove_sea()
+    get_picture_dates_and_location()
+    #model(train_df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
